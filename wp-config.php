59810d16b261c4343c9524b6e08359a98bdc9064
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-09-04 11:09:34 +0000

    config database

--- wp-config.php
+++ wp-config.php
@@ -19,12 +19,13 @@
  *
  * @package WordPress
  */
-if (isset($_ENV["DATABASE_URL"]))
+if (isset($_ENV["DATABASE_URL1"]))
 {
     define('DB_NAME', $_ENV["DB_NAME"]);
     define('DB_USER', $_ENV["DB_USER"]);
     define('DB_PASSWORD', $_ENV["DB_PASSWORD"]);
     define('DB_HOST', $_ENV["DB_HOST"]);
+    define('DB_PORT', $_ENV["DB_PORT"]);
 }
 else
 {
