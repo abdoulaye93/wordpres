80b3ec5f6fb3dc0a73edd91bf7ef8d2c5e5430e2
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-09-04 10:37:24 +0000

    config database

--- wp-config.php
+++ wp-config.php
@@ -94,5 +94,18 @@ define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) )
   define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
+
+if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
+    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
+    define(‘DB_NAME’, trim($db[`path`],`/`));
+    define(‘DB_USER’, $db[`user`]);
+    define(‘DB_PASSWORD’, $db[`pass`]);
+    define(‘DB_HOST’, $db[`host`]);
+    define(‘DB_CHARSET’, ‘utf8’);
+    define(‘DB_COLLATE’, ‘’);
+} else {
+    die('No Database credentials!');
+}
+
 /** Réglage des variables de WordPress et de ses fichiers inclus. */
 require_once( ABSPATH . 'wp-settings.php' );
